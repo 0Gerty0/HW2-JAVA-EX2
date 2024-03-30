39f14d5a97b70d9dd8095807c1ea104a065eadab
Aleksandr Danilushkin <thenumber23@ya.ru>
2024-03-30 12:05:54 +0300

    Edits

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         int balance = 1000;// Объявляете переменные для входных данных и
-        int bonus = 0;
-        int payment = 1230;// параметров программы: начального счёта,
-        if (payment > 1000){
-            ;// суммы пополнения и тп
+        int bonus = 0;// параметров программы: начального счёта, суммы пополнения и тп
+        int payment = 1230;
+        if (payment > 1000) {
+
             bonus = payment / 100;
         }
-        balance = balance+bonus;
+        balance = balance + bonus;
         System.out.println(bonus);
         System.out.println(balance);
         // Условным оператором проверяете, превысила ли
